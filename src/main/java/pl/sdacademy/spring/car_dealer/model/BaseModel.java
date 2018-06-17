c88161d588047bc244c035e6128e22495bdb8854
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-06-17 10:48:43 +0200

    Mapped entities

--- src/main/java/pl/sdacademy/spring/car_dealer/model/BaseModel.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/model/BaseModel.java
@@ -1,9 +1,15 @@
 package pl.sdacademy.spring.car_dealer.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class BaseModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version
+    private Long version;
 
     public Long getId() {
         return id;
@@ -13,4 +19,11 @@ public abstract class BaseModel implements Serializable {
         this.id = id;
     }
 
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
 }
